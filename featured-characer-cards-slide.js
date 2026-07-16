96e606c28c1484cd526ecaa3f5aa1fbf587357c1
Nemxfacami <ettnemxfacami671@gmail.com>
2026-07-16 14:52:50 +0200

    Update featured-characer-cards-slide.js

--- featured-characer-cards-slide.js
+++ featured-characer-cards-slide.js
@@ -1,15 +1,14 @@
-
 function showImage(index, btn){
     document
         .querySelectorAll(".feature-character-card")[index]
         .scrollIntoView({
             behavior: "smooth",
+            block: "nearest",
             inline: "start"
         });
 
     document
         .querySelectorAll(".feature-buttons-style")
         .forEach(button => button.classList.remove('activeSlide'));
-        btn.classList.add('activeSlide');
-        
+    btn.classList.add('activeSlide');
 }
