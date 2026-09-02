9a018676fb40310cc0826821bef7eb27755f9613
Nemxfacami <ettnemxfacami671@gmail.com>
2026-09-02 17:36:33 +0200

    adding html

--- ravenport-blogs/page-3/pathchange.py
+++ ravenport-blogs/page-3/pathchange.py
@@ -4,53 +4,32 @@ import re
 # Current folder
 folder = "."
 
-# File extensions and their target folders
-folders = {
-    ".css": "css",
-    ".js": "js",
-}
-
-image_extensions = (
-    ".png", ".jpg", ".jpeg", ".gif", ".webp",
-    ".svg", ".ico", ".bmp", ".avif"
-)
-
 # Match src="..." and href="..."
 attribute_pattern = re.compile(
     r'(?P<attr>\b(?:src|href)\s*=\s*)(?P<quote>["\'])(?P<path>.*?)(?P=quote)',
     re.IGNORECASE
 )
 
-def fix_path(path):
-    path = path.strip()
-
-    # Don't touch URLs, anchors, data, protocol-relative paths, etc.
+def remove_folder(path):
+    # Don't touch URLs, anchors, data, etc.
     if (
         not path
-        or path.startswith(("http://", "https://", "//", "#", "data:", "mailto:", "tel:"))
+        or path.startswith((
+            "http://", "https://", "//",
+            "#", "data:", "mailto:", "tel:"
+        ))
     ):
         return path
 
-    # Separate query string / fragment
-    match = re.match(r'([^?#]*)(.*)', path)
-    clean_path = match.group(1)
-    suffix = match.group(2)
+    # Remove images/, css/, or js/
+    if path.startswith("images/"):
+        return path[len("images/"):]
 
-    # Don't touch paths that already have the correct folder
-    if clean_path.startswith(("images/", "css/", "js/")):
-        return path
+    if path.startswith("css/"):
+        return path[len("css/"):]
 
-    filename = os.path.basename(clean_path)
-
-    # Images
-    if filename.lower().endswith(image_extensions):
-        return "images/" + clean_path + suffix
-
-    # CSS / JS
-    extension = os.path.splitext(filename)[1].lower()
-
-    if extension in folders:
-        return folders[extension] + "/" + clean_path + suffix
+    if path.startswith("js/"):
+        return path[len("js/"):]
 
     return path
 
@@ -67,10 +46,11 @@ for filename in os.listdir(folder):
         content = file.read()
 
     new_content = attribute_pattern.sub(
-        lambda m: m.group("attr") +
-                  m.group("quote") +
-                  fix_path(m.group("path")) +
-                  m.group("quote"),
+        lambda m:
+            m.group("attr")
+            + m.group("quote")
+            + remove_folder(m.group("path"))
+            + m.group("quote"),
         content
     )
 
